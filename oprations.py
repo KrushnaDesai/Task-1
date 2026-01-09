65c3e8fedb7837a4e9e90d072a525ee440bec3ab
KrushnaDesai <krushnadesai21@gmail.com>
2026-01-10 02:49:36 +0530

    Add files via upload
    Task 1: Perform Basic Mathematical Operations
    Task 2: Create a Personalized Greeting

---
+++ oprations.py
@@ -0,0 +1,12 @@
+a=float(input("Enter the first number:"))
+b=float(input("Enter the second number:"))
+
+sum=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+
+print("addition:",sum)
+print("subtraction:",subtraction)
+print("multiplication:",multiplication)     
+print("division:",division) 
